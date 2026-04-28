6dde7f52e6bcb631689e527b92b6f6343d644b72
sophiaparisi <sophiaparisi@vt.edu>
2026-04-28 16:20:50 -0400

    spotify april top 10

---
+++ spotify-json/spotify-charts.js
@@ -0,0 +1,377 @@
+const spotifyChart =
+// paste code from Canvas below //
+{
+  "countryFilters": [],
+  "displayChart": {
+    "date": "2026-04-23",
+    "chartMetadata": {
+      "uri": "spotify:chart:7zE64clO2DB2y74QymNWRn",
+      "alias": "REGIONAL_GLOBAL_WEEKLY",
+      "entityType": "TRACK",
+      "readableTitle": "Weekly Top Songs: Global",
+      "backgroundColor": "#773795",
+      "textColor": "#FFFFFF",
+      "dimensions": {
+        "latestDate": "2026-04-23",
+        "earliestDate": "2016-12-29",
+        "country": "GLOBAL",
+        "chartType": "TOP_TRACK",
+        "recurrence": "WEEKLY",
+        "genre": "",
+        "city": ""
+      }
+    },
+    "description": "Your weekly update of the most played tracks right now."
+  },
+  "entries": [
+    {
+      "chartEntryData": {
+        "currentRank": 1,
+        "previousRank": 3,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_UP",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Beauty And A Beat",
+        "trackUri": "spotify:track:6QFCMUUq1T2Vf5sFUXcuQ7",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02f1d02a6cec967f8b6b78f76e",
+        "artists": [
+          {
+            "name": "Justin Bieber",
+            "spotifyUri": "spotify:artist:1uNFoZAHBGtllmzznpCI3s",
+            "externalUrl": ""
+          },
+          {
+            "name": "Nicki Minaj",
+            "spotifyUri": "spotify:artist:0hCNtLu0JehylgoiP8L4Gh",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 2,
+        "previousRank": -1,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "NEW_ENTRY",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "drop dead",
+        "trackUri": "spotify:track:6gkbtMtioHgtyGjrMel6ei",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02c25b53f0a34aa1bb4f77cb3e",
+        "artists": [
+          {
+            "name": "Olivia Rodrigo",
+            "spotifyUri": "spotify:artist:1McMsnEElThX1knmY4oliG",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 3,
+        "previousRank": 1,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_DOWN",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "SWIM",
+        "trackUri": "spotify:track:68lbSrXDORS51pmyjZv712",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02dfa17fad7f190c901603270e",
+        "artists": [
+          {
+            "name": "BTS",
+            "spotifyUri": "spotify:artist:3Nrfpe0tUJi4K4DXYWgMUX",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 4,
+        "previousRank": 2,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_DOWN",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Babydoll",
+        "trackUri": "spotify:track:7yNf9YjeO5JXUE3JEBgnYc",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e027b1b6f41c1645af9757d5616",
+        "artists": [
+          {
+            "name": "Dominic Fike",
+            "spotifyUri": "spotify:artist:6USv9qhCn6zfxlBQIYJ9qs",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 5,
+        "previousRank": 20,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_UP",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "DAISIES",
+        "trackUri": "spotify:track:5BZsQlgw21vDOAjoqkNgKb",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02d65c4773bc5061fd27facc5b",
+        "artists": [
+          {
+            "name": "Justin Bieber",
+            "spotifyUri": "spotify:artist:1uNFoZAHBGtllmzznpCI3s",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 6,
+        "previousRank": 21,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_UP",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Baby",
+        "trackUri": "spotify:track:6epn3r7S14KUqlReYr77hA",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02629dc9e2e3bc20bbd7d92e4a",
+        "artists": [
+          {
+            "name": "Justin Bieber",
+            "spotifyUri": "spotify:artist:1uNFoZAHBGtllmzznpCI3s",
+            "externalUrl": ""
+          },
+          {
+            "name": "Ludacris",
+            "spotifyUri": "spotify:artist:3ipn9JLAPI5GUEo4y4jcoi",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 7,
+        "previousRank": 4,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_DOWN",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Risk It All",
+        "trackUri": "spotify:track:5y2ijHECwFYWqcAHKTZgzD",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e023eb8dc748f7efb1470f74395",
+        "artists": [
+          {
+            "name": "Bruno Mars",
+            "spotifyUri": "spotify:artist:0du5cEVh5yTK9QJze8zA0C",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 8,
+        "previousRank": 9,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_UP",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Dracula - JENNIE Remix",
+        "trackUri": "spotify:track:5yvVYFDUpbnjcnRBgjwTzM",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02c7c031ce9d06b131f8563676",
+        "artists": [
+          {
+            "name": "Tame Impala",
+            "spotifyUri": "spotify:artist:5INjqkS1o8h1imAzPqGZBb",
+            "externalUrl": ""
+          },
+          {
+            "name": "JENNIE",
+            "spotifyUri": "spotify:artist:250b0Wlc5Vk0CoUsaCY84M",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 9,
+        "previousRank": 26,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_UP",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Confident",
+        "trackUri": "spotify:track:6czM6UMQD9qE5GAUp68oud",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e0258ae8fddecbd2630005409c9",
+        "artists": [
+          {
+            "name": "Justin Bieber",
+            "spotifyUri": "spotify:artist:1uNFoZAHBGtllmzznpCI3s",
+            "externalUrl": ""
+          },
+          {
+            "name": "Chance the Rapper",
+            "spotifyUri": "spotify:artist:1anyVhU62p31KFi8MEzkbf",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    },
+    {
+      "chartEntryData": {
+        "currentRank": 10,
+        "previousRank": 8,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_DOWN",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "back to friends",
+        "trackUri": "spotify:track:7qjZnBKE73H4Oxkopwulqe",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e024229702de91cb3d3a4383302",
+        "artists": [
+          {
+            "name": "sombr",
+            "spotifyUri": "spotify:artist:4G9NDjRyZFDlJKMRL8hx3S",
+            "externalUrl": ""
+          }
+        ],
+        "producers": [],
+        "labels": [],
+        "songWriters": [],
+        "releaseDate": ""
+      }
+    }
+  ]
+}
+
+document.getElementById('week').innerHTML = `Top 10 on Spotify for <span class="green">${spotifyChart.displayChart.date}</span>`;
+document.getElementById('list').innerHTML =
+  spotifyChart.entries.map(spotifyData => {
+    return `<li class="track">
+    <div>
+      <img class="albumCover" src="${spotifyData.trackMetadata.displayImageUri}">
+    </div>
+    <div class="track-info">
+      <div class="rank">
+        <div class="current">${spotifyData.chartEntryData.currentRank}</div>
+      </div>
+      <div class="credits">
+        <div class="title">${spotifyData.trackMetadata.trackName}</div>
+        <div class="artists">${spotifyData.trackMetadata.artists.map(creators => { return creators.name }).join('\n')}</div>
+        <div class="listen"><a href="${spotifyData.trackMetadata.trackUri.replace("spotify:track:", "https://open.spotify.com/track/")}" target="_blank">Listen on Spotify</a></div>
+      <div>
+    </div>
+  </li>`
+  }).join('');
+
+function narrow() {
+  document.getElementById('filter').innerHTML = " ";
+  var search = document.getElementById('search').value;
+  var tracks = document.getElementsByClassName('track');
+  var arr = Array.from(tracks);
+  var filt = arr.map(e => e.outerHTML);
+  document.getElementById('filter').innerHTML = filt.filter(f => f.match(search)).join('')
+}
---
+++ spotify-json/style.css
@@ -0,0 +1,105 @@
+html {
+  font-family: 'Helvetica Neue', 'Arial', sans-serif;
+  font-size: 1.2rem;
+  line-height: 1.2rem;
+  background-color: #ececec;
+}
+
+#container {
+  /* max-width: 100%; */
+  margin: 5rem auto;
+}
+
+h1 {
+  font-size: 1.7rem;
+  line-height: 1.7rem;
+  padding: .5rem;
+}
+
+.green {
+  color: #1DB954;
+}
+
+#list, #filter {
+  margin: 2rem auto;
+} 
+
+li {
+  border-bottom: solid 1px #aeaeae;
+  /* box-shadow: 1px 1px #aeaeae;
+  background-color: #f9f9f9;
+  margin: 15px auto; */
+}
+
+li:last-of-type {
+  border-bottom: none;
+}
+
+.track {
+  display: flex;
+  align-items: center;
+}
+
+.track > div {
+  padding: .5rem;
+}
+
+.albumCover {
+  width: 5rem;
+  border: solid 1px #aeaeae;
+  display: flex;
+}
+
+.track-info {
+  display: flex;
+  align-items: center;
+}
+
+.rank {
+  width: 0;
+  margin: 1rem 1rem 1rem 2rem;
+  display: flex;
+  justify-content: right;
+}
+
+.current {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+.prev {
+  font-size: 1rem;
+}
+
+.title {
+  font-weight: bold;
+}
+
+.artists {
+  font-size: 0.8rem;
+  color: #404040;
+}
+
+.listen {
+  display: flex;
+}
+
+.listen a {
+  font-size: 0.6rem;
+  color: #773795;
+  border: solid 1px #773795;
+  margin-top: 5px;
+  padding: 0 10px;
+  border-radius: 5px;
+  text-decoration: none;
+}
+
+@media screen and (min-width: 768px) {
+  html {
+      font-size: 1.6rem;
+  }
+
+  #container {
+      max-width: 768px;
+  }
+}
